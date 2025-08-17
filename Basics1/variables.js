ed4dbbb2d27d6deddece3001d22c13fbdda2b904
Ranjeetadas00 <100126029+Ranjeetadas00@users.noreply.github.com>
2025-08-17 02:56:35 +0000

    variables and DT

---
+++ Basics1/variables.js
@@ -0,0 +1,15 @@
+const accId =1443
+let accEmail = "rd@gmail.com";
+var accPw = "1235" ////"var" is not used in morden days programming because of block scope and functional scope
+accCity = "Jaipur";
+var state
+
+console.log(accCity);
+
+accEmail = "ranjee@gmail.com"
+accPw = "6789"
+accCity = "Bangalore"
+
+console.log(accEmail);
+
+console.table([accEmail, accPw, accCity,state ])
