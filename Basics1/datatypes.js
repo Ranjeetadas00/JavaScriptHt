076dfec581cff497afe1b618eebc798e5f86f846
Ranjeetadas00 <ranjeecg@gmail.com>
2025-08-18 16:50:09 +0530

    dataTypes

--- Basics1/datatypes.js
+++ Basics1/datatypes.js
@@ -2,28 +2,72 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+// console.log(3 
+//     +
+//      3) // code readability should be high
 
-console.log("Hitesh")
-
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
-
-// number => 2 to power 53
-// bigint
-// string => ""
-// boolean => true/false
-// null => standalone value
-// undefined => 
-// symbol => unique
+// console.log("Ranjeeta")
 
 
-// object
+// let name = "Ranjeeta"
+// let age = 18
+// // let isLoggedIn = false
+// let state;
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value (User Assigned value)
+// // undefined => 
+// // symbol => unique (mostly used in React and Figma to give symbols to components)
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+// // =============================================================
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // false -> because
+
+// const bigNumber = 3456543576654356754n
+// for primitive types the reference variables are created in Stack Memory and when we assign a primitive value to another variable => then it creates a new 
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}
